9f08969474407fcf77aacc4838347187f0abc8d7
melanie silva <melsil11@hotmail.com>
2022-11-11 10:50:02 -0500

    added css and converted header.js to nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import Projects from './Projects'
 import {Link} from "react-router-dom"
+import { Nav } from 'react-bootstrap'
 
 
 
 const Header = () => {
     return (
-        <div className='header-container'>
-            <div>
+        <container className='header-container'>
+            <Nav.Item className = 'p-links'>
                 <Link to='/' className='heading-container'>
-                    <h1 className='header-name'>Melanie Silva</h1>
+                    <p className='header-name'>Melanie Silva</p>
                 </Link>
-            </div>
-            <div className = 'links'>
+            </Nav.Item>
+            <Nav.Item  className = 'p-links'>
                 <Link to='/aboutme' className='p-links'>
                     <p>About Me</p>
                 </Link>
+            </Nav.Item>
+            <Nav.Item  className = 'p-links'>
                 <Link to='/projects' className='p-links'>
                     <p>Projects</p>
                     {/* need to add link address for the updated resume */}
                 </Link>
+            </Nav.Item>  
+            <Nav.Item  className = 'p-links'>  
                 <p><a href='' target="_blank" className='p-links'>Resume</a></p>
+            </Nav.Item>
+            <Nav.Item  className = 'p-links'>
                 <p><a href='https://www.linkedin.com/in/melanie-silva-ms' target="_blank" className='p-links'>LinkedIn</a></p>
+            </Nav.Item>
+            <Nav.Item  className = 'p-links'>
                 <p><a href='https://github.com/melsil11' target="_blank" className='p-links'>Github</a></p>
-            </div>
-        </div>
+            </Nav.Item >
+        </container>
     )
 } 
  
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,7 +10,9 @@ const HomePage = () => {
             <div className='intro'>
                 <p className='hello'>Hi!</p>
                 <p className='hello'>I'm Melanie Silva</p>
-                <p className='welcome-description'></p>
+                <p className='welcome-description'>I am a Fullstack Software Engineer with a passion for creating a more accessible world through code. My code is greatly influenced by my many years as a special education teacher developing ideas to make the world around my students more accessible and meeting each individuals needs.</p>
+                <p className='welcome-description'> Let's make a change:</p>
+                <p className='welcome-description'> melanie.silva2121@gmail.com </p>
             </div>
         </div>
         </>
