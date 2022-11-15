8b3b8ba0ddc4d9db57b7fd5b3779567b70a5e795
melanie silva <melsil11@hotmail.com>
2022-11-15 08:25:08 -0500

    css image about  me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
             <div>
                 <h1 className='about-me' id= 'about-me-heading'>Hello and thanks for visiting!</h1>
                 <p className='about-me'> My name is Melanie Silva, and I am a Fullstack Software Engineer that is experienced in creating fullstack applications that meet the needs and expectations of clients. </p>
-                <p className='about-me'> My ability to effectively communicate with my team and customers comes from my many years of experience working as a teacher which enabled me to expertly develop meaningful curriculum.  </p>
+                <p className='about-me'> My ability to effectively communicate with my team and customers comes from my many years of experience working as a teacher which enabled me to expertly develop meaningful curriculum.</p>
             </div>
             </div>
         </div>
