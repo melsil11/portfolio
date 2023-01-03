0cbbd6b57a4fdde6c6dd279e0815b06dec776f44
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-01-03 18:19:55 -0500

    added skills and adjusted padding

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,6 +7,7 @@ const AboutMe = () => {
             <img src={ Picture } className='image' alt='collageimage'/>
             <div>
                 <h1 className='about-me' id= 'about-me-heading'>Hello and thanks for visiting!</h1>
+                <span class='span'></span>
                 <p className='about-me'> My name is Melanie Silva, and I am a Fullstack Software Engineer that is experienced in creating fullstack applications that meet the needs and expectations of clients. </p>
                 <p className='about-me'> My ability to effectively communicate with my team and customers comes from my many years of experience working as a teacher which enabled me to expertly develop meaningful curriculum.</p>
                 <p className='about-me'> For fun I enjoy spending as much time as I can outside, whether it is hiking with my dogs or harvesting my own produce. Many of my hobbies emulate why I love coding. I enjoy taking raw materials and building things out of them such as crafting and carpentry. </p>
