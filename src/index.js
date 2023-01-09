d1b2de8696ba3c2fa1f91fe4f3e5c19e76d79516
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-01-09 18:54:23 -0500

    styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
