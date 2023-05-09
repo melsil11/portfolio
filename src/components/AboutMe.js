5d1d7ff8bd5f0ed070f6939e259193bc1ec85c9e
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-05-08 20:27:59 -0400

    changed collage image to center on mobile devices

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,9 +12,9 @@ const AboutMe = () => {
     }, []);
     return (
         <div>
-            <div className='homepage-container'>
-            <img src={ Picture } className='image' alt='collageimage'/>
+            <div className='homepage-container'>           
             <div>
+                <img src={ Picture } className='collageImage' alt='collage'/>
                 <h1 className='about-me' id= 'about-me-heading'>Hello and thanks for visiting!</h1>
                 <span className='span'></span>
                 <p className='about-me'> My name is Melanie Silva, and I am a Fullstack Software Engineer that is experienced in creating fullstack applications that meet the needs and expectations of clients. </p>
